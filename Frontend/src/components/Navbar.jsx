472994a6c8f69a41cc4d6a6d79176593e9ce110b
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-04-16 17:00:54 +0300

    update navbar component

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <img src={assets.logo} className="w-36" alt="logo" />
+      <Link to='/'><img src={assets.logo} className="w-36" alt="logo" /></Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700 ">
         <NavLink to='/' className="flex flex-col items-center gap-1"> 
@@ -49,17 +49,17 @@ const Navbar = () => {
         <img onClick={() => setVisisble(true)} src={assets.menu_icon} className="w-5 cursor-pointer sm:hidden " alt="menu icon" />
       </div>
 
-      <div className={`absolute top-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+      <div className={`fixed top-0 bottom-0 scroll-m-0 overflow-hidden bg-white transition-all z-50 ${visible ? 'w-full' : 'w-0'}`}>
         <div className="flex flex-col text-gray-600">
           <div onClick={() => setVisisble(false)} className="flex items-center gap-4 p-3 cursor-pointer">
             <img src={assets.dropdown_icon} className="h-4 rotate-180" alt="drop down" />
             <p>Back</p>
           </div>
 
-          <NavLink onClick={()=> setVisisble(false)} className="py-2 pl-4 mr-4 border mb-2" to="/">HOME</NavLink>
-          <NavLink onClick={()=> setVisisble(false)} className="py-2 pl-4 mr-4 border mb-2" to="/collection">COLLECTION</NavLink>
-          <NavLink onClick={()=> setVisisble(false)} className="py-2 pl-4 mr-4 border mb-2" to="/about">ABOUT</NavLink>
-          <NavLink onClick={()=> setVisisble(false)} className="py-2 pl-4 mr-4 border mb-2" to="/contact">CONTACT</NavLink>
+          <NavLink onClick={()=> setVisisble(false)} className="py-2 pl-4 border mb-2" to="/">HOME</NavLink>
+          <NavLink onClick={()=> setVisisble(false)} className="py-2 pl-4 border mb-2" to="/collection">COLLECTION</NavLink>
+          <NavLink onClick={()=> setVisisble(false)} className="py-2 pl-4 border mb-2" to="/about">ABOUT</NavLink>
+          <NavLink onClick={()=> setVisisble(false)} className="py-2 pl-4 border mb-2" to="/contact">CONTACT</NavLink>
         </div>
       </div>
     </div>
